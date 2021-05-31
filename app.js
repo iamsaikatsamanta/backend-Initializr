e8af130b28cf23d89fbc9bb7779d1d27b42406b6
Saikat Samanta <33680333+iamsaikatsamanta@users.noreply.github.com>
2021-05-31 17:05:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/api/admin', adminApiRouter);
