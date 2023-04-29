f4af40fbac76cbefbd6ac9ea4c9cc95e5b332aa1
Saikat Samanta <33680333+iamsaikatsamanta@users.noreply.github.com>
2023-04-29 15:25:35 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const urlencodedParser = bodyParser.urlencoded({extended: true});
 const UserAuth = require('../controller/UserAuthController');
 
 router.post('/create-user', urlencodedParser, UserAuth.createUser);
-router.get('/get-user', urlencodedParser, UserAuth.getUser);
+// router.get('/get-user', urlencodedParser, UserAuth.getUser);
 router.post('/update-user', urlencodedParser, UserAuth.UpdateUser);
 router.post('/create-course', urlencodedParser, UserAuth.CreateCourse);
 router.get('/get-all-course', urlencodedParser, UserAuth.getAllCourse);
